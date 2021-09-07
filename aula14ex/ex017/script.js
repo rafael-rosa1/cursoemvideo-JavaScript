2b962a1dc66a6a65929fded7c4dce8e392943a81
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-09-07 14:12:27 -0300

    exercicio tabuada pronto

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert("ola")
+function calcular() {
+    let numero = document.querySelector('input#numero')
+    let tabuada = document.getElementById('seltab')
+    
+    if (numero.value.length == 0) {
+        alert("Digite um número para descobrir sua tabuada")
+    } else {
+        let n = Number(numero.value)
+        let i = 1
+        tabuada.innerHTML = ""
+        while(i <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${i} = ${i*n}`
+            item.value = `tab${i}`
+            tabuada.appendChild(item)
+            i++
+        }
+    }
+}
