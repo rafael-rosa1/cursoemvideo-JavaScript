9472e0f4ed6fd43d05b1b98f3364f8d27647a762
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-09-06 21:15:06 -0300

    fim controle repetiçao

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var contador = 1
+while (contador <= 5) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+console.log('')
+
+var contador2 = 1
+do {
+    console.log(`PassoDO ${contador2}`)
+    contador2++
+} while (contador2 <= 5)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
